07dead913cbc41ae9c8ace5ab0cdc9bec96e1db4
praneeth2497 <40980571+praneeth2497@users.noreply.github.com>
2019-03-01 17:37:15 +0530

    Add files via upload

---
+++ abstractexample/abstractexample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstractexample
+{
+    abstract class AbstractExample
+    {
+        public abstract void msg();
+    }
+    class AbstracT1 : AbstractExample
+    {
+        public override void msg()
+        {
+            Console.WriteLine("hello");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
